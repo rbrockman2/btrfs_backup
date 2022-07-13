1835e95bcf595c8dbd45264e1a3f19abb7b8a45d
root <root@wizard.freeexpansion.net>
2022-07-13 00:32:57 -0500

    Added lockfile to prevent backups from stepping on each other.

--- bin/all_external_backup.sh
+++ bin/all_external_backup.sh
@@ -2,6 +2,15 @@
 # Cron sometimes has the wrong path.
 export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin/btrfs_backup"
 
+# Check for lock.
+if [[ -e "/run/backup.lock" ]]; then
+    echo "Backup already in progress, aborting."
+    exit 1
+fi
+
+# Create lock.
+touch "/run/backup.lock"
+
 source_subvol_list="/etc/btrfs_backup/source_subvol_list.txt"
 
 mount_backup.sh
@@ -11,3 +20,6 @@ do
     external_backup.sh $subvol
 done
 umount_backup.sh
+
+# Remove lock.
+rm "/run/backup.lock"
