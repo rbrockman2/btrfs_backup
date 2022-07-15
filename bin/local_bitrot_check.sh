6d5b1ecc0f489de624be4b5d05d1775043111073
Robert Theron Brockman II <robert@firehead.org>
2022-07-15 00:44:58 -0500

    Fixed bitrot check to use filesystems rather than subvolumes.

--- bin/local_bitrot_check.sh
+++ bin/local_bitrot_check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin/btrfs_backup"
 
-source_subvol_list="/etc/btrfs_backup/source_subvol_list.txt"
-for subvol in `cat $source_subvol_list`
+btrfs_filesystem_list="/etc/btrfs_backup/btrfs_filesystem_list.txt"
+for filesystem in `cat $btrfs_filesystem_list`
 do
-    btrfs scrub start -B $subvol > /var/log/local_bitrot_check.log
+    btrfs scrub start -B $filesystem > /var/log/local_bitrot_check.log
 done
