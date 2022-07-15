91cb837102eaa9220c1226ea6d0090fbf1e03930
Robert Theron Brockman II <robert@firehead.org>
2022-07-15 01:02:33 -0500

    Fixed bug in bitrot check that overwrote log.

--- bin/local_bitrot_check.sh
+++ bin/local_bitrot_check.sh
@@ -4,5 +4,5 @@ export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin/btrfs_backup"
 btrfs_filesystem_list="/etc/btrfs_backup/btrfs_filesystem_list.txt"
 for filesystem in `cat $btrfs_filesystem_list`
 do
-    btrfs scrub start -B $filesystem > /var/log/local_bitrot_check.log
+    btrfs scrub start -B $filesystem >> /var/log/local_bitrot_check.log
 done
